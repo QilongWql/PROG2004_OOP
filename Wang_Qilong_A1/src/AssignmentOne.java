9d5488ed71a77d7cdd61943d45a7a38bd5b21d3d
QilongWql <wql15652335666@163.com>
2024-11-14 19:07:19 +0800

    Part 1 Base class_2024_11_14
    Part 1 - Base class is written.

--- Wang_Qilong_A1/src/AssignmentOne.java
+++ Wang_Qilong_A1/src/AssignmentOne.java
@@ -1,5 +1,34 @@
 public class AssignmentOne {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
+        HealthProfessional hp1 = new HealthProfessional(12, "Tom", "Roud123", "123445677");
+        HealthProfessional hp2 = new HealthProfessional(13, "PFDFHom", "Roud1FFS3", "127");
+        hp1.printDetails();
+        hp2.printDetails();
+    }
+}
+
+class HealthProfessional{
+    public int ID;
+    public String name;
+    public String phoneNum;
+    public String address;
             
-        }
+    public HealthProfessional(){
+            this.ID = 0;
+            this.name = "";
+            this.phoneNum = "";
+            this.address = "";
+    }
+    public HealthProfessional(int ID, String name, String phoneNum, String address){
+            this.ID = ID;
+            this.name = name;
+            this.phoneNum = phoneNum;
+            this.address = address;
+    }
+    public void printDetails(){
+            System.out.println("ID: " + this.ID);
+            System.out.println("Name: " + this.name);
+            System.out.println("Phone number: " + this.phoneNum);
+            System.out.println("Address: " + this.address);
+    }
 }
